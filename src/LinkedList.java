8739d1bab24ed8c6cc31f99ead21733b6f6ce728
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-07-27 00:04:08 +0530

    basic operation in linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,9 @@
-public class LL {
+public class LinkedList {
+    private int size;
+
+    LinkedList(){
+        this.size=0;
+    }
 
     class Node{  //A node is represented as a class ,A basic node structure is created
         //properties of a node
@@ -9,6 +14,7 @@ public class LL {
         Node(String data){
             this.data=data;
             this.next=null;
+            size++;
         }
     }
     private Node head;
@@ -43,7 +49,7 @@ public class LL {
     }
     //print
     public void printList(){
-        if(head==null){//corner case to check if node doesn't exists 
+        if(head==null){//corner case to check if node doesn't exists
             System.out.println("list is empty");
             return;
         }
@@ -56,8 +62,42 @@ public class LL {
         System.out.println("Null");
     }
 
+    //delete first
+    public void deleteFirst(){
+        if(head==null){//corner case to check if node doesn't exists
+            System.out.println("list is empty");
+            return;
+        }
+        size --;
+        head=head.next;
+    }
+
+    //delete last
+    public void deleteLast(){
+        if(head==null){//corner case to check if node doesn't exists
+            System.out.println("list is empty");
+            return;
+        }
+        size --;
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        Node secondLast=head;
+        Node lastNode=head.next;
+        while(lastNode.next!=null){
+            lastNode=lastNode.next;
+            secondLast=secondLast.next;
+        }
+        secondLast.next=null;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
     public static void main(String[] args){
-        LL list =new LL();//object of LL
+        LinkedList list =new LinkedList();//object of LL
         list.addFirst("jiggy");
         list.addFirst("piggy");
         list.printList();
@@ -68,6 +108,15 @@ public class LL {
         list.addFirst("Hey");
         list.printList();
 
+        list.deleteFirst();
+        list.printList();
+
+        list.deleteLast();
+        list.printList();
+
+        System.out.println(list.getSize());
+        list.addFirst("new node");
+        list.printList();
 
 
     }
